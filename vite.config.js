cc4cb24b86312b56d33b26e7e780f404ce0cb7da
Mfundo <mfundobgwebu101@gmail.com>
2023-05-29 12:53:44 +0200

    first commit

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import handlebars from 'vite-plugin-handlebars';
+import data from "./data.json" assert {type: 'json'};
+
+
+const basicContext = {
+};
+
+    export default {
+      plugins: [handlebars({
+        context : data
+    })]
+  };
